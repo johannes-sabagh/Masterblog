422ffb29baf24a70df34a49262b603f9384f84a5
johannes-sabagh <johannes.sabagh@gmail.com>
2026-03-10 17:57:08 +0100

    added update

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from importlib.resources import contents
+
 from flask import Flask, render_template, request, redirect, url_for
 import json
 
@@ -54,6 +56,39 @@ def delete(post_id):
 
     return redirect(url_for('index'))
 
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    post = fetch_post_by_id(post_id)
+    if post is None:
+        return "Post not found", 404
 
+    if request.method == 'POST':
+        author = request.form['author']
+        title = request.form['title']
+        content = request.form['content']
+
+        with open('blog_posts.json', 'r') as posts_file:
+            blog_posts = json.load(posts_file)
+        for p in blog_posts:
+            if p['id'] == post_id:
+                p['author'] = author
+                p['title'] = title
+                p['content'] = content
+                break
+
+        with open('blog_posts.json', 'w') as posts_file:
+            json.dump(blog_posts, posts_file, indent=4)
+
+        return redirect(url_for('index'))
+
+    return render_template('update.html', post=post)
+
+def fetch_post_by_id(post_id):
+    with open('blog_posts.json', 'r') as posts_file:
+        blog_posts = json.load(posts_file)
+    for post in blog_posts:
+        if post['id'] == post_id:
+            return post
+    return None
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
