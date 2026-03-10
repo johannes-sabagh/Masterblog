90a1c7280c4c632542dd1ae17a01251c4941a2e8
johannes-sabagh <johannes.sabagh@gmail.com>
2026-03-10 12:24:20 +0100

    added JSON and updated the index to fetch the post authors

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # add code here to fetch the job posts from a file
+    with open('blog_posts.json', 'r') as posts_file:
+        blog_posts= json.load(posts_file)
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
